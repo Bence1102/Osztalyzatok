899aec3579a7021761c0746081421bf65e77d3af
Bence1102 <szente.bence04@gmail.com>
2024-10-24 10:37:12 +0200

    jegyek atlaga

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import feladatok
 
 parogramozas_jegyek=[5,2,3,4,4,5,5,5,1,2,2,3,4,5,5,5,4,3,3]
+
+print("1.feladat:")
+print(f"Jegyek átlaga: {feladatok.jegyek_atlag(parogramozas_jegyek)}")
+print("")
