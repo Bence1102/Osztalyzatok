c22489ed692c75e08afb29c57b359ad1a0e9832f
Vktr22 <nds.viktoria@gmail.com>
2024-10-24 11:20:18 +0200

    fhfh

--- feladatok.py
+++ feladatok.py
@@ -28,13 +28,27 @@ def jegyek_atlag(lista):
 
 
 
-def sordiagarm(lista): #eljaras
+def savdiagarm(lista): #eljaras
+    i:int=0
+    while(i<len(lista)):
+        csillag:int=0
 
+        print(f"{i}| {csillag}")
+        i+=1
 
 def jegyek_kiir(lista): #eljaras
-
+    i:int=0
+    while(i<len(lista)):
+        print(f"{i}. diák: {lista[i]}")
+        i+=1
 
 def veletlen_jegyek(lista): #eljaras
+    lista=lista[]
+    i:int=0
+    while(i<17)
+        jegy:int=random.randint()*4+1
+        lista[i]=jegy
+        i+=1
 
 
 
